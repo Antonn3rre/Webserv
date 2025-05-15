226a772b2e91997b38f37d0172ba0d6b6500b66f
Attra <macuesta@student.42.fr>
2025-05-15 15:26:30 +0200

    modification to test without the parsing

--- inc/Server.hpp
+++ inc/Server.hpp
@@ -1,20 +1,21 @@
 #ifndef SERVER_HPP
 # define SERVER_HPP
 
-# include "Config.hpp"
+// # include "Config.hpp"
 # include <sys/socket.h>
 # include <netinet/in.h>
 
 class Server {
 	private:
-		Config	_config;
+		// Config	_config;
 		int		_msocket;
-		struct sockaddr_in _server_addr;
+		struct sockaddr_in	_server_addr;
 
 	public:
 		Server(void);
-		Server(char *configFile);
+		// Server(char *configFile);
 		void	startServer(void);
+		void	handleClient(void);
 };
 
 #endif // SERVER_HPP
--- src/server/Server.cpp
+++ src/server/Server.cpp
@@ -7,9 +7,11 @@
 #include <stdlib.h>
 #include <string>
 
-Server::Server(void) : _config(Config("conf/default.conf")) {};
+Server::Server(void) : _client_addr_size(sizeof(_client_addr)) {};
+// Server::Server(void) : _config(Config("conf/default.conf")) {};
 
-Server::Server(char *configFile) : _config(Config(configFile)) {};
+// Server::Server(char *configFile) {};
+// Server::Server(char *configFile) : _config(Config(configFile)) {};
 
 void Server::startServer(void) {
 	_msocket = socket(AF_INET, SOCK_STREAM, 0);
@@ -18,15 +20,26 @@ void Server::startServer(void) {
 		exit(1);
 	}
 	_server_addr.sin_family = AF_INET;
-	_server_addr.sin_port = htons(std::atoi(_config.getHost().c_str()));
+	_server_addr.sin_port = htons(std::atoi("8080"));
 	_server_addr.sin_addr.s_addr = htonl(INADDR_ANY); // INADDR_ANY: server listens on all available local interfaces
+
 	if (bind(_msocket, (struct sockaddr*)&_server_addr, sizeof(_server_addr)) < 0) {
 		std::cerr << "Failed to bind server socket." << std::endl;
 		exit(1);
 	}
+
 	if (listen(_msocket, 5) < 0) {
 		std::cerr << "Failed to listen on server socket." << std::endl;
 		exit(1);
 	}
-	std::cout << "Server started on port: " << _port << std::endl;
+	std::cout << "Server started on port: " << "8080" << std::endl;
+	handleClient();
+}
+
+void	Server::handleClient(void) {
+	_client_socket_fd = accept(_msocket, (struct sockaddr *)&_client_socket_fd, &_client_addr_size);
+	if (_client_socket_fd < 0) {
+		std::cerr << "Failed to accept client request." << std::endl;
+		exit(1);
+	}
 }
