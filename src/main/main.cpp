1e6cb5df1c8cdc56ac24802ceea86016ae797c75
Anton G <antongozlan@hotmail.com>
2025-06-05 14:48:05 +0200

    Ajout check exception main pour parsing config

--- src/main/main.cpp
+++ src/main/main.cpp
@@ -3,7 +3,11 @@
 
 int main() {
 	std::cout << "\e[33mTEST START SERVER\e[0m" << std::endl;
-	Server test("conf/defaultWithoutCommentaries.conf");
-	test.startServer();
-	return 0;
+  try {
+	  Server test("conf/defaultWithoutCommentaries.conf");
+	  test.startServer();
+  } catch (Config::Exception &e) {
+    std::cout << e.what() << std::endl;
+  }
+  return 0;
 }
