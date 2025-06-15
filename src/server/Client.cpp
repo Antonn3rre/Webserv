973ee317ee5013d22283546edcb1e74527a9a281
agozlan <agozlan@student.42.fr>
2025-06-15 15:01:43 +0200

    fichiers Client

---
+++ src/server/Client.cpp
@@ -0,0 +1,32 @@
+#include "Client.hpp"
+#include "Application.hpp"
+#include "Server.hpp"
+
+Client::Client()
+    : _clientfd(-1), _status(READING_INPUT), _bytesToRead(-1), _bytesWritten(0), _chunk(false) {}
+
+Client::Client(int clientfd)
+    : _clientfd(clientfd), _status(READING_INPUT), _bytesToRead(-1), _bytesWritten(0),
+      _chunk(false) {}
+
+Client::~Client() {};
+
+Client &Client::operator=(const Client &rhs) {
+	if (this != &rhs) {
+		_clientfd = rhs._clientfd;
+		_status = rhs._status;
+		_bytesToRead = rhs._bytesToRead;
+		_bytesWritten = rhs._bytesWritten;
+		_bufferRead = rhs._bufferRead;
+		_bufferWrite = rhs._bufferWrite;
+		_chunk = rhs._chunk;
+		_application = rhs._application;
+	}
+	return *this;
+}
+
+void Client::setApplication(Application *application) { _application = application; }
+
+Application &Client::getApplication() const { return *_application; }
+
+int Client::getClientfd(void) const { return _clientfd; }
